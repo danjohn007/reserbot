840345e627b06b27327fd1fd3c028adb60d091b9
IMPACTOS DIGITALES <77515822+danjohn007@users.noreply.github.com>
2025-11-12 08:44:55 -0600

    Update database configuration values

--- config/config.php
+++ config/config.php
@@ -27,8 +27,8 @@ define('BASE_PATH', dirname(dirname(__FILE__)));
 
 // Database Configuration
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'i45com_reserbot');
-define('DB_USER', 'i45com_reserbot');
+define('DB_NAME', 'recaudab_reserbot');
+define('DB_USER', 'recaudab_reserbot');
 define('DB_PASS', 'Danjohn007!');
 define('DB_CHARSET', 'utf8mb4');
 
